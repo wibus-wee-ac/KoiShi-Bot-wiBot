047007e8e8170bd5484d26c3d7d28b3fe58611d7
wibus-wee <1596355173@qq.com>
2021-08-25 08:52:45 +0800

    :tada: Begin Koishi Bot project!

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @FilePath: /KoiShi-Bot-wiBot/src/index.ts
+ * @author: Wibus
+ * @Date: 2021-08-25 07:38:53
+ * @LastEditors: Wibus
+ * @LastEditTime: 2021-08-25 07:42:40
+ * Coding With IU
+ */
+function _index() {
+    const list = {}
+    return list
+}
+export default _index
---
+++ src/ping.ts
@@ -0,0 +1,9 @@
+/*
+ * @FilePath: /KoiShi-Bot-wiBot/src/ping.ts
+ * @author: Wibus
+ * @Date: 2021-08-25 07:43:33
+ * @LastEditors: Wibus
+ * @LastEditTime: 2021-08-25 07:45:37
+ * Coding With IU
+ */
+import { Context } from "koishi-core";
